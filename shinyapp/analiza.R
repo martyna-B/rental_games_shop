8a48e1bc01e5492fd3b0eaac01fa07fd6fe7c1fa
Malgorzata Jakubik <262279@student.pwr.edu.pl>
2023-06-14 22:55:40 +0200

    Top 10 zawodników

--- shinyapp/analiza.R
+++ shinyapp/analiza.R
@@ -1,7 +1,8 @@
-install.packages("RMariaDB")
+# install.packages("plotly")
 library(RMariaDB)
 library(tidyverse)
 library(dbplyr)
+library(plotly)
 
 con_mariadb <- dbConnect(RMariaDB::MariaDB(),
                 dbname = 'team03',
@@ -9,5 +10,57 @@ con_mariadb <- dbConnect(RMariaDB::MariaDB(),
                 password = "te@m0e",
                 host = "giniewicz.it")
 
-score <- tbl(con_mariadb, "score")
+score <- tbl(con_mariadb, "score") %>%
+        collect()
 
+tournament <- tbl(con_mariadb, "tournament") %>%
+        select(game_id, tournament_id, tournament_date) %>%
+        collect()
+
+customer <- tbl(con_mariadb, "customer") %>%
+        select(first_name, last_name, customer_id) %>%
+        collect()
+
+game <- tbl(con_mariadb, "game") %>%
+        select(game_id, game_title) %>%
+        collect()
+
+
+# 10 z najlepszym score dla każdej grupy (zwraca więcej niż 10, bo remisy)
+score_tournament <- left_join(score, tournament, by = c("tournament_id")) %>%
+    group_by(game_id) %>%
+    slice_max(score, n = 10) %>%
+    select(!c(score_id))
+
+
+### join
+# join z customer by customer_id, żeby wiedzieć kto ma jakiego score
+top_10_customers_in_general <- left_join(score_tournament, customer, by=c("customer_id")) %>%
+        select(!c(customer_id))
+
+# join z game by game_id żeby wiedzieć jaki tytuł miała gra
+
+
+top_10_customers_in_general <- top_10_customers_in_general %>% mutate(full_name = str_c(first_name, " ", last_name))
+top_10_customers_in_general <- top_10_customers_in_general %>% mutate(text_plot = str_c("score: ", score, " - ", tournament_date))
+
+
+### plots
+generate_plots <- function(data_arg, game_id_arg){
+        data <- data_arg %>% filter(game_id == game_id_arg %>% as.numeric()) %>% top_n(10)
+        text <- data$text_plot
+
+        fig <- plot_ly(data, x = ~score, y = ~full_name, text = text,
+                type = 'bar', orientation = 'h', textposition = 'auto',
+                marker = list(color = "#44bd44"))
+        fig <- fig %>% layout(title = paste0("Top 10 graczy w ", game_id_arg),
+                xaxis = list(title = ""),
+                yaxis = list(title = "", categoryorder = "total ascending"))
+
+        fig
+}
+
+# top_10_grouped <- top_10_customers_in_general %>% group_by(game_id) %>% tally()
+# for (i in top_10_grouped$game_id){
+#         generate_plots(data_arg = top_10_customers_in_general, game_id_arg = i)  
+# }
--- shinyapp/app.R
+++ shinyapp/app.R
@@ -1,7 +1,10 @@
 library(shiny)
-library(shinythemes)
 library(shinydashboard)
 library(rmarkdown)
+library(tidyverse)
+source("analiza.R", local=T)
+# library(webshot)
+# webshot::install_phantomjs()
 
 # Workaround for https://github.com/yihui/knitr/issues/1538
 if (packageVersion("knitr") < "1.17.3") {
@@ -16,9 +19,9 @@ if (packageVersion("knitr") < "1.17.3") {
 
 
 ui <- dashboardPage(skin='green',
-  dashboardHeader(title = "Gametopia Rentals"),
+  dashboardHeader(title = "Geeks & Dragons"),
   dashboardSidebar(
-    radioButtons('format', 'Document format', c('HTML'),
+    radioButtons('format', 'Aby pobrać raport naciśnij przycisk Download', c('HTML'),
                    inline = TRUE),
       downloadButton('downloadReport')
   ),
@@ -31,7 +34,20 @@ ui <- dashboardPage(skin='green',
               uiOutput('pracownik_miesiaca')
             ),
             tabPanel("Turniej - top 10 zawodników",
-              uiOutput('turniej_top_10')
+              dateRangeInput("dates_top_10", label = "Wybierz zakres dat, z którego chcesz poznać top 10 zawodników",
+                 start  = "2020-01-01",
+                 end    = "2023-12-31",
+                 min    = "2020-01-01",
+                 max    = "2023-12-31",
+                 format = "mm/dd/yy",
+                 separator = " - "),
+
+              uiOutput('turniej_top_10'),
+              plotlyOutput("plot_top_10_1"),
+              plotlyOutput("plot_top_10_2"),
+              plotlyOutput("plot_top_10_3"),
+              plotlyOutput("plot_top_10_4"),
+              plotlyOutput("plot_top_10_5")
             ),
             tabPanel("Dochody",
               uiOutput('dochody')
@@ -62,6 +78,40 @@ server <- function(input, output) {
     )
   })
 
+  output$value <- renderPrint({ input$dates_top_10 })
+  
+  generate_plots <- function(game_id_arg) {
+    data <- top_10_customers_in_general %>% filter(game_id == game_id_arg %>% as.numeric()) %>% top_n(10)
+    text <- data$text_plot
+    
+    fig <- plot_ly(data, x = ~score, y = ~full_name, text = text,
+                   type = 'bar', orientation = 'h', textposition = 'auto',
+                   marker = list(color = "#44bd44"))
+    fig <- fig %>% layout(title = paste0("Top 10 graczy w ", game_id_arg),
+                          xaxis = list(title = ""),
+                          yaxis = list(title = "", categoryorder = "total ascending"))
+    
+    fig
+    }
+  
+  
+  output$plot_top_10_1 <- plotly::renderPlotly({
+    generate_plots(1)
+  })
+  output$plot_top_10_2 <- plotly::renderPlotly({
+    generate_plots(2)
+  })
+  output$plot_top_10_3 <- plotly::renderPlotly({
+    generate_plots(3)
+  })
+  output$plot_top_10_4 <- plotly::renderPlotly({
+    generate_plots(4)
+  })
+  output$plot_top_10_5 <- plotly::renderPlotly({
+    generate_plots(5)
+  })
+  
+
   output$turniej_top_10 <- renderUI({
     src <- normalizePath('turniej_top_10.Rmd')
 
@@ -72,12 +122,12 @@ server <- function(input, output) {
         # syntax highlighting
         tags$script(HTML("if (hljs) $('#report pre code').each(function(i, e) {
                             hljs.highlightBlock(e)
-                        });"))
-    )
+                        });")))
     })
 
     output$dochody <- renderUI({
     src <- normalizePath('dochody.Rmd')
+
 
     tagList(
         HTML(knitr::knit2html(text = readLines(src), template = FALSE)),
